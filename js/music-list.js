b9b18c77b884b4fff99fcddcd54d4a2ca5a286d8
Ashfaque Hossain Abir <106385010+abirett2@users.noreply.github.com>
2022-09-22 08:45:45 +0545

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Lost Stories-Bombay Dreams",
+    artist: "KSHMR",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "The World We Left Behind-KSHMR",
+    artist: "KSHMR & KARRA",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "KSHMR & Mahmut Orhan- Slow ",
+    artist: "KSHMR,Mahmut Orhan & KARRA",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "KSHMR X Tungevaag - Close Your Eyes",
+    artist: "KSHMR & Tungevaag",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Mahmut Orhan & Sena- Fly above",
+    artist: "Mahmut Orhan & Sena",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Mahmut Orhan- Save me",
+    artist: "Mahmut Orhan & Eneli",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
